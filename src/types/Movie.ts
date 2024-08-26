e89e5f2ade4f5960da4a637d728a1c0f423e01c6
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 19:31:45 -0300

    feat: movie interface

---
+++ src/types/Movie.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    release_date: string;
+    overview: string;
+    vote_average: number;
+    genre_ids: number[];
+}
+
+export interface MovieResponse {
+    results: Movie[];
+}
