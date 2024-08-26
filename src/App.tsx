731aef5800c3426b9ea98eab21866ad1fbdbdda3
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 19:44:27 -0300

    feat: create movie list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from "react"
 import './App.css'
+import { MovieList } from "./components/MovieList"
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
+    <MovieList/>
   )
 }
 
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Movie } from "../types/Movie";
+import { getPopularMovies } from "../services/movieApiService";
+
+export function MovieList() {
+
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        const fetchMovies = async() => {
+            const popularMovies = await getPopularMovies();
+            setMovies(popularMovies);
+        }
+        fetchMovies();
+    }, []);
+    
+    return (
+        <div>
+            <h1 className="text-4xl	font-semibold">Popular Movies</h1>
+            <p className="text-xl text-gray-500 mt-2">Find out what's trending in the world of cinema.</p>
+        </div>
+    )
+}
