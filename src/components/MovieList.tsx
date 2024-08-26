a5be52565c2847d6e91a5e36e2c8fef2a81b8fd4
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 19:45:04 -0300

    refactor: remove react import

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Movie } from "../types/Movie";
 import { getPopularMovies } from "../services/movieApiService";
 
