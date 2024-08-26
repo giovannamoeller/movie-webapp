e583b1af937ff8daa5c25bc3e16f7c90364ce5d7
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 20:42:42 -0300

    refactor: add return type

--- src/services/movieApiService.ts
+++ src/services/movieApiService.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { tmdbApiKey } from "../keys/tmdb_api_key";
+import { Movie } from "../types/Movie";
 
 const apiKey: string = tmdbApiKey
 const baseUrl: string = 'https://api.themoviedb.org/3'
 
-export const getPopularMovies = async () => {
+export const getPopularMovies = async (): Promise<Movie[]> => {
     try {
         const response = await axios.get(
             `${baseUrl}/movie/popular`, {
@@ -16,6 +17,6 @@ export const getPopularMovies = async () => {
         return response.data.results;
     } catch (error) {
         console.error("Error fetching popular movies", error);
-        //return [];
+        return [];
     }
 };
