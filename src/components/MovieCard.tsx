31b2d0462b769d77b128822c67d3b66fe6f28cc2
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 21:12:55 -0300

    feat: add genre enum type

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,21 @@
 import { Movie } from "../types/Movie"
+import { Genres } from "../types/Genre";
 
-interface MovieCardProps {
+type MovieCardProps = {
     movie: Movie;
-}
+};
 
 export function MovieCard({ movie }: MovieCardProps) {
 
-    const { title, poster_path, vote_average, overview } = movie;
+    const { title, poster_path, vote_average, overview, genre_ids } = movie;
+
+    function getGenre() {
+        let genres: string = '';
+        for (let i = 0; i < Math.min(genre_ids.length, 2); i++) {
+            genres += Genres[genre_ids[i]] + ', ';
+        }
+        return genres.slice(0, -2);
+    }
 
     return (
         <div>
@@ -16,7 +25,7 @@ export function MovieCard({ movie }: MovieCardProps) {
                 className="w-full h-72 object-cover"/>
             <div className="text-left">
                 <p className="text-xl font-bold mt-4 line-clamp-1">{title}</p>
-                <p className="text-lg text-cyan-900 mt-1">Mystery, {vote_average.toFixed(1)}</p>
+                <p className="text-lg text-cyan-900 mt-1">{getGenre()}, {vote_average.toFixed(1)}</p>
                 <p className="mt-4 text-neutral-700 line-clamp-3">{overview}</p>
             </div>
         </div>
