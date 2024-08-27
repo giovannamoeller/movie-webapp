514de9ad5a73e40f220cbe5fac937c30c09f4764
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-26 22:34:34 -0300

    fix: add object top to movie card image

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -18,7 +18,7 @@ export function MovieCard({ movie }: MovieCardProps) {
             <img 
                 src={`https://image.tmdb.org/t/p/w500${poster_path}`}
                 alt={`${title} poster`}
-                className="w-full h-72 object-cover"/>
+                className="w-full h-72 object-cover object-top"/>
             <div className="text-left">
                 <p className="text-xl font-bold mt-4 line-clamp-1">{title}</p>
                 <p className="text-lg text-cyan-900 mt-1">{getGenre()}, {vote_average.toFixed(1)}</p>
