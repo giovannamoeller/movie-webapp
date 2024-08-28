ed15a15de6b414b99a6751727403e0562dd422e8
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-27 21:12:01 -0300

    feat: add animation to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,8 +7,8 @@ type ModalProps = {
 };
 
 export function Modal({ children, isOpen, onClose }: ModalProps) {
-    return isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-70 backdrop-blur-xl flex justify-center items-center">
+    return (
+        <div className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-xl flex justify-center items-center transition-all duration-300 ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`} >
             <div className="bg-white rounded-md">
                 <IoCloseOutline className="cursor-pointer ml-auto text-2xl mt-4 mr-4" onClick={onClose}/>
                 {children}
