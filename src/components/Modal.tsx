abeb90313ee0750d9b93092c0edbc933ad20c6f0
giovannamoeller <giovannagmoeller@gmail.com>
2024-08-27 21:24:44 -0300

    feat: close modal when clicking outside

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEvent, useEffect } from "react";
 import { IoCloseOutline } from "react-icons/io5";
 
 type ModalProps = {
@@ -8,8 +9,10 @@ type ModalProps = {
 
 export function Modal({ children, isOpen, onClose }: ModalProps) {
     return (
-        <div className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-xl flex justify-center items-center transition-all duration-300 ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`} >
-            <div className="bg-white rounded-md">
+        <div 
+            className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-xl flex justify-center items-center transition-all duration-300 ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`} 
+            onClick={onClose}>
+            <div className="bg-white rounded-md" onClick={(event: React.MouseEvent) => event.stopPropagation()}>
                 <IoCloseOutline className="cursor-pointer ml-auto text-2xl mt-4 mr-4" onClick={onClose}/>
                 {children}
             </div>
